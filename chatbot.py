3671ff46e56defaa5560dcecc0f99ab917ada398
ardhitosen <christoforusardhito@gmail.com>
2025-06-14 22:03:01 +0700

    Refactor chatbot.py to update import paths for Chroma and TextLoader, ensuring compatibility with langchain_community modules.

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 from langchain.document_loaders import TextLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
 from openai import OpenAI
 import os
 from fastapi import FastAPI, HTTPException
